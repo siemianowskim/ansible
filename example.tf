68ca25299b89ba42332983a0fcfccb84a19f20cb
Michal <siemianowski.michal@gmail.com>
2016-11-06 13:51:56 +0000

    ...

--- example.tf
+++ example.tf
@@ -13,7 +13,7 @@ resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = "${aws_vpc.vpc.id}"
 }
 
-# Give the VPC internet access on its main route table
+# Give the VPC internet access on its main route table.
 resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
